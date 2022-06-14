37e4af8356b349e148482222674f24bc641151da
ta <ntparis9@gmail.com>
2022-06-14 11:06:57 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+  <div class="min-h-screen">
+    <Nav />
+
+    <main class="py-[5%] flex items-center justify-between text-center flex-col">
+      <section class="prose">
+        <h1 class="title-primary">Welcome to the restaurantly</h1>
+        <a class="">Go to restaurants</a>
+      </section>
+
+      <section>
+        <p class=""></p>
+        <div class="">
+          <template>
+            <div class="">
+              <h2 class=""></h2>
+              <ul class="">
+                <template><li class=""></li></template>
+              </ul>
+              <ul class="">
+                <li class=""><NuxtLink>Get Started</NuxtLink></li>
+              </ul>
+            </div>
+          </template>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
---
+++ pages/restaurants.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen">
+    <Nav />
+
+    <main class="py-[5%] flex items-center justify-between text-center flex-col">
+      <section class="prose">
+        <h1 class="title-primary">Hello this is the restaurant</h1>
+        <a class="">Go to restaurants</a>
+      </section>
+    </main>
+  </div>
+</template>
